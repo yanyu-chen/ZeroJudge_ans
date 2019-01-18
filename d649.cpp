c304e39a2cc0a35d098286619f29f0051e811e71
Yan-Yu Chen <33624871+yanyu-chen@users.noreply.github.com>
2019-01-18 16:56:42 +0800

    Add files via upload

--- d649.cpp
+++ d649.cpp
@@ -3,20 +3,16 @@ using namespace std;
 
 int main(){
 	int n;
-
 	while(cin >> n && n != 0){
 		int a = n;
-
 		while (n > 0){
 			int i = 1;
 
 			while (i < n){
 				cout << "_";
 				i += 1;
-			}
-			
+			}	
 			int j = a - i + 1;
-			
 			while (j > 0){
 				cout << "+";
 				j -= 1;
